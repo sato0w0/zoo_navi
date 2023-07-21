c4a441182583eb597840c61f7a12a85ce2e905a4
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-07-21 13:32:29 +0000

    [Update]レイアウト調整中

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -12,6 +12,14 @@ class Animal < ApplicationRecord
     image.variant(resize_to_limit: [width, height]).processed
   end
 
+  def get_image_fit(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_fit: [width, height]).processed
+  end
+
   def self.search_for(word)
     unless word.blank?
       Animal.where("name LIKE ?", "%#{word}%")
--- app/models/zoo.rb
+++ app/models/zoo.rb
@@ -11,6 +11,14 @@ class Zoo < ApplicationRecord
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def get_image_fit(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_fit: [width, height]).processed
+  end
 
   def self.search_for(content, method)
     if method == 'perfect'
