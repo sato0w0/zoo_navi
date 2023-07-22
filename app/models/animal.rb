1b1a51c05459a74b4a097e1f05a5dd23d14900ff
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-07-22 17:44:34 +0000

    [Update]レイアウト調整

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -19,6 +19,14 @@ class Animal < ApplicationRecord
     end
     image.variant(resize_to_fit: [width, height]).processed
   end
+  
+  def get_image_fill(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_fill: [width, height]).processed
+  end
 
   def self.search_for(word)
     unless word.blank?
